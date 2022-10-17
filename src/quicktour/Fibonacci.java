d8d2eb8b2ddd0d91403727d28542142cb17d4ddc
don <kdj00001@naver.com>
2022-10-17 14:15:22 +0900

    add Fibonacci

---
+++ src/quicktour/Fibonacci.java
@@ -0,0 +1,30 @@
+package quicktour;
+
+class Fibonacci {
+	/** Print out the Fibonacci sequence for values < 50 */
+	public static void main(String args[]) {
+		int lo = 1;
+		int hi = 1;
+
+		System.out.println(lo);
+		while (hi < 50) {
+			System.out.println(hi);
+			hi = lo + hi;	// new hi
+			lo = hi - lo;	/* new lo is (sum - old lo)
+							   i.e., the old hi */
+		}
+	}
+}
+/*
+ *	Java built-in primitive data types
+ *
+ *	boolean either true or false
+ *	char	16-bit Unicode 2.0 charactor
+ *	byte	8-bit integer (signed)
+ *	short	16-bit integer (signed)
+ *	int		32-bit integer (signed)
+ *	long	64-bit integer (signed)
+ *	float	32-bit floating-point (IEEE 754-1985)
+ *	double	62-bit floating-point (IEEE 754-1985)
+ *
+ */
---
+++ src/quicktour/HelloWorld.java
@@ -0,0 +1,15 @@
+package quicktour;
+
+public class HelloWorld {
+	public static void main(String args[]) { // args 는 CLI 로 해당 Java 파일을 실행할 때 넣어주는 매개변수이다.
+		System.out.println("Hello, world");
+	}
+}
+/*
+ *	# CLI 
+ *  
+ *  Compile : javac *(file name).java
+ * 
+ *  Run : java HelloWorld(only file name)
+ * 
+ */
