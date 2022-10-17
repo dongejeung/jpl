86122fda8e755d4a9d1e99ec9485222f445a1f15
don <kdj00001@naver.com>
2022-10-17 18:53:06 +0900

    ex1.7

---
+++ src/quicktour/Exer1_7.java
@@ -0,0 +1,10 @@
+package quicktour;
+
+public class Exer1_7 {
+	/** Backward Loop example */
+	public static void main(String[] args) {
+		for(int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
