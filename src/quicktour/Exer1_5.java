16da7a8d3e83bbe3ef5fe5d0ee5f6ecff648a762
don <kdj00001@naver.com>
2022-11-11 15:11:30 +0900

    rename Excer -> Exer

--- src/quicktour/Exer1_5.java
+++ src/quicktour/Exer1_5.java
@@ -1,8 +1,32 @@
 package quicktour;
 
-public class Exer1_5 {
-	final static String SAY_HELLO = "Hello, world";
-	public static void main(String[] args) {
-		System.out.println(SAY_HELLO);
+class Exer1_11 {
+	/** Print out the Fibonacci sequence for values < 50 */
+	public static void main(String args[]) {
+		int lo = 1;
+		int hi = 1;
+
+		int cnt = 1;
+		System.out.println(lo);
+		while (hi < 50) {
+			System.out.println(cnt +"seq : "+ hi);
+			hi = lo + hi;	// new hi
+			lo = hi - lo;	/* new lo is (sum - old lo)
+							   i.e., the old hi */
+			cnt++;
+		}
 	}
 }
+/*
+ *	Java built-in primitive data types
+ *
+ *	boolean either true or false
+ *	char	16-bit Unicode 2.0 charactor
+ *	byte	8-bit integer (signed)
+ *	short	16-bit integer (signed)
+ *	int		32-bit integer (signed)
+ *	long	64-bit integer (signed)
+ *	float	32-bit floating-point (IEEE 754-1985)
+ *	double	62-bit floating-point (IEEE 754-1985)
+ *
+ */
